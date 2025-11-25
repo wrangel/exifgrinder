032f4f3d53c985139a1bc059f2ebe88ae8e5f713
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2025-11-25 11:27:37 +0100

    Update misc utilities

--- src/main/scala/ch/wrangel/toolbox/utilities/MiscUtilities.scala
+++ src/main/scala/ch/wrangel/toolbox/utilities/MiscUtilities.scala
@@ -1,3 +1,5 @@
+// utlities/MiscUtilities.scala
+
 package ch.wrangel.toolbox.utilities
 
 import ch.wrangel.toolbox.Constants
@@ -7,79 +9,52 @@ import java.io.IOException
 import java.net.URI
 import java.nio.file.Paths
 import org.htmlcleaner.{HtmlCleaner, TagNode}
-import scala.collection.mutable.{ListBuffer}
+import scala.collection.mutable.ListBuffer
 import scala.io.StdIn
 import scala.sys.process.{Process, ProcessLogger}
 import scala.util.{Try, Success, Failure}
 import wvlet.log.LogSupport
 
-/* Utilities for miscellaneous functionality */
+/** Utilities for miscellaneous functionality */
 object MiscUtilities extends LogSupport {
 
-  /** Splits a collection into multiple subsets, according to the [[Seq]] of indices provided
-    *
-    * @param splitPoints [[Seq]] of indices
-    * @param s [[String]] to split
-    * @param result [[ListBuffer]] containing the subsets
-    */
-  def splitCollection(splitPoints: Seq[Int],
-                      s: String,
-                      result: ListBuffer[String]): Unit = {
+  /** Splits a string into multiple subsets by provided indices (tail-recursive) */
+  def splitCollection(splitPoints: Seq[Int], s: String, result: ListBuffer[String]): Unit = {
     val (element, rest) = s.splitAt(splitPoints.head)
-    if (rest.nonEmpty)
-      splitCollection(splitPoints.tail, rest, result)
+    if (rest.nonEmpty) splitCollection(splitPoints.tail, rest, result)
     element +=: result
   }
 
-  /** Handles shell processes
-    * https://stackoverflow.com/questions/29935873/how-to-run-external-process-in-scala-and-get-both-exit-code-and-output
-    *
-    * @param command [[String]] representing shell command
-    * @return Optional Stdout
-    */
+  /** Executes a shell command and returns optional stdout */
   def getProcessOutput(command: String): Option[String] = {
-    val stdout: StringBuilder = new StringBuilder
-    val stderr: StringBuilder = new StringBuilder
-    val procLogger: ProcessLogger = ProcessLogger { 
-      line => 
-        stdout.append(line + "\n")
-        stderr.append(line + "\n")
-    }
-    Process(command.stripMargin).!(procLogger) match {
-      case 0 =>
-        Some(stdout.toString.trim)
-      case _ =>
-        info(stderr.toString.trim)
-        None
+    val stdout = new StringBuilder
+    val stderr = new StringBuilder
+    val logger = ProcessLogger(
+      (o: String) => stdout.append(o + "\n"),
+      (e: String) => stderr.append(e + "\n")
+    )
+    val exitCode = Process(command.stripMargin).!(logger)
+    if (exitCode == 0) Some(stdout.toString.trim)
+    else {
+      info(stderr.toString.trim)
+      None
     }
   }
 
-  /** Requests correct user input
-    *
-    * @param message Message to the user
-    * @param validRange [[Seq]] of valid values
-    * @return Correct user input
-    */
+  /** Requests correct user input from valid options (tail-recursive) */
   @scala.annotation.tailrec
   def getFeedback(message: String = "", validRange: Seq[String]): String = {
-    val addendum: String =
-      s"Please select one of (${validRange.mkString(", ")})\n"
-    val feedback: String = StdIn.readLine(message.trim + "\n" + addendum)
-    if (validRange.contains(feedback))
-      feedback
-    else
-      getFeedback(validRange = validRange)
+    val addendum = s"Please select one of (${validRange.mkString(", ")})\n"
+    val feedback = StdIn.readLine(message.trim + "\n" + addendum)
+    if (validRange.contains(feedback)) feedback else getFeedback(validRange = validRange)
   }
 
-  /** Currying function returning the ExifTool version present on the local computer
-   *
-   * @return Current ExifTool version in Double format
-   */
+  /** Returns current installed ExifTool version as Double */
   def getPresentExifToolVersion: Double = getProcessOutput(
     s"${Constants.ExifToolBaseCommand.split(Constants.BlankSplitter).head.trim} -ver"
   ).getOrElse("-1").toDouble
 
-  /** Installs a new or updated version of ExifTool */
+  /** Checks and installs or updates ExifTool if newer version available */
   def handleExifTool(): Unit = {
     try {
       val url = URI.create(Constants.ExifToolWebsite).toURL()
@@ -92,7 +67,7 @@ object MiscUtilities extends LogSupport {
           inputStream.close()
         }
       } catch {
-        case e: IOException => 
+        case e: IOException =>
           error(s"Error reading from URL: ${e.getMessage}")
           return
       }
@@ -104,13 +79,11 @@ object MiscUtilities extends LogSupport {
           val text = element.getText.toString
           val versionRegex = """ExifTool-(\d+\.\d+)\.pkg""".r
           versionRegex.findFirstMatchIn(text).map(_.group(1))
-        } else {
-          None
-        }
+        } else None
       }.headOption
 
       macPkgVersion match {
-        case Some(version) => 
+        case Some(version) =>
           val newestVersion = version.toDouble
           val presentVersion = getPresentExifToolVersion
           if (presentVersion < newestVersion) {
@@ -120,10 +93,8 @@ object MiscUtilities extends LogSupport {
             Try {
               FileUtilities.download(s"${Constants.ExifToolWebsite}/$pkgName", downloadPath)
             } match {
-              case Success(_) =>
-                info(s"Newest ExifTool version ($newestVersion) is now downloaded")
-              case Failure(e) =>
-                error(s"Failed to download ExifTool: ${e.getMessage}")
+              case Success(_) => info(s"Newest ExifTool version ($newestVersion) downloaded")
+              case Failure(e) => error(s"Failed to download ExifTool: ${e.getMessage}")
             }
           } else {
             info(s"Current ExifTool version ($presentVersion) is up to date")
@@ -140,5 +111,4 @@ object MiscUtilities extends LogSupport {
         error(s"Unexpected error: ${e.getMessage}")
     }
   }
-
 }
